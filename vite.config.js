1422d15b852816da50ee5d4c8a56a7b544504e23
Frankebuka <frankebuka@yahoo.com>
2024-05-20 02:07:25 +0000

    fixed

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://medibook-1.netlify.app",
         secure: false,
       },
     },
